e852c9d5eaba8dcbc389eb46df3da1467793a839
mateuszGorczany <mateusz.g64@interia.pl>
2023-01-30 03:06:51 +0100

    update port

--- main.py
+++ main.py
@@ -49,6 +49,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=int(os.getenv("SERVICE_PORT", 8080)),
+        port=int(os.getenv("WEBSITES_PORT", 8080)),
         log_level="info",
     )
